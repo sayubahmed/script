62f674944b38767ad485f4b77dcb0500797feebd
sayubahmed <ayub58377@gmail.com>
2023-12-22 06:57:47 -0800

    hello

---
+++ one.js
@@ -0,0 +1,12 @@
+function ayub(azam,ali){
+  console.log("repair work is done")
+  azam();
+  ali();
+}
+function a (){
+    console.log("call customer")
+}
+function v (){
+    console.log("print bill")
+}
+ayub (a,v)
